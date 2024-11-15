1c266377538144bd0c013027ade26f143fec5c7d
Smithapadayattil <smithapadayattil@gmail.com>
2024-11-15 20:31:24 +0530

    Add files via upload

---
+++ Assigmnt1.py
@@ -0,0 +1,45 @@
+#Exercise 1
+print("smitha")
+print(1123056)
+print("smithapadayattil@gmail.com")
+#Exercise 2
+print('\"Smitha\"\n1123056\nsmithapadayattil@gmail.com')
+#Exercise 3
+print(14+7)
+print(14-7)
+print(14*7)
+print(14/7)
+#exercise 4
+for i in range(1,6):
+    print(i)
+#Exercise 5
+print('\"SDK\" stands for \"Software Development Kit\"')
+print('\"IDE\" stands for \"Integrated Development Environment\"')
+#Exercise 6
+print("python is an \"awesome\" language.")
+print("python\n\t2023")
+print('I\'m from Entri.\b')
+print("\65")
+print("\x65")
+print("Entri", "2023", sep="\n")
+print("Entri", "2023", sep="\b")
+print("Entri", "2023", sep="*", end="\b\b\b\b")
+#Exercise 7
+num=23
+textnum="57"
+decimal=98.3
+print(type(num))
+print(type(textnum))
+print(type(decimal))
+#Exercise 8
+year=365
+minutes=60
+hours=24
+total_minutes=year*minutes*hours
+print(total_minutes)
+#Exercise 9
+name1=(input("Please enter your Name:"))
+print(f"Hi {name1}, Welcome to python programming :)")
+#Exercise 10
+l=int(input("Please enter the amount in pounds :"))
+print(f"{l} pounds are {l*1.27} Dollars")
